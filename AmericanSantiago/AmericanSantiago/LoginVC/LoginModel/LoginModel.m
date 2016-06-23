67cce986e9113ee77bf796e46d2c78264606360e
IRVINzoetory <Mervin@MervindeMBP.lan>
2016-06-23 23:12:39 +0800

    备注
    这里可以不写

--- AmericanSantiago/AmericanSantiago/LoginVC/LoginModel/LoginModel.m
+++ AmericanSantiago/AmericanSantiago/LoginVC/LoginModel/LoginModel.m
@@ -29,51 +29,6 @@
         }
     }];
     
-    // Create manager
-//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-//    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-//    
-//    // JSON Body
-//        NSDictionary* bodyObject = @{
-//    
-//                                             @"username":username,
-//                                             @"password":password,
-//                                             
-//                                     };
-//
-//    
-////    NSDictionary * dic1 = @{@"username":@"123456",@"password":@"123456",@"nickname":@"mwk",@"birthday":@"19930102",@"gender":@"0",@"character":@"boy"};
-//    
-//    NSString * urlString = @"/Login";
-//    NSString * URL = [NSString stringWithFormat:@"%@%@",BASE_URL,urlString];
-//    
-//    NSMutableURLRequest* request = [[AFJSONRequestSerializer serializer] requestWithMethod:@"POST"
-//                                                                                 URLString:URL
-//                                                                                parameters:bodyObject
-//                                                                                     error:NULL];
-//    
-//    //    NSLog(@"===============%@",bodyObject);
-//    //    NSLog(@"+++++++++++++++%@",request);
-//    
-//    // Fetch Request
-//    AFHTTPRequestOperation *operation = [manager HTTPRequestOperationWithRequest:request
-//                                                                         success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//                                                                             
-//                                                                             
-//                                                                             NSString *string = [[NSString alloc]initWithData:responseObject encoding:NSUTF8StringEncoding];
-//                                                                             //                                                                             NSLog(@"fuck- ----- %@",string);
-//                                                                             id object = [string  objectFromJSONString];
-//                                                                             self.loginData = object;
-//                                                                             
-//                                                                             NSLog(@"HTTP Response Status Code: %ld", [operation.response statusCode]);
-//                                                                             NSLog(@"HTTP Response Body  loginData == : %@", object);
-//                                                                             
-//                                                                         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//                                                                             NSLog(@"HTTP Request failed: %@", error);
-//                                                                         }];
-//    
-//    [manager.operationQueue addOperation:operation];
-
 }
 
 
